9149982c9ce3af77135e80d169607465b6e0c9c0
[DCTomblinson] <[fb40yf@gmail.com]>
2020-04-20 05:31:44 -0500

    Cleaned up main further

--- Program4.java
+++ Program4.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 /**
  * Has user select a graph file theavailable options, then performs Prim's min spanning tree algorithm and writes the resulting graph file to results.txt
  * @author Devon Tomblinson
@@ -9,27 +8,9 @@ public class Program4 {
         //Print introductory messages and options
         System.out.println("Welcome to Program4");
         System.out.println("By: Devon Tomblinson");
-        Decider decider = new Decider();
-
-        //Use scanner and boolean to set up user input
-        Scanner sc = new Scanner(System.in);
-        Boolean done = false;
-
-        while(!done){
-            //print directions
-            decider.directions();
-            
-            //Get user input
-            String input = sc.nextLine();
-
-            //Use the file that the user chooses or exit depending on the input
-            decider.execute(input);
-            
-            //close the scanner and end the program
-            if(input.equals("Exit")){
-            sc.close();
-            System.exit(0);
-            }
-        }
+        
+        //run user interface
+        Input in = new Input();
+        in.run();
     }
 }
